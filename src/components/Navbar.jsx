c3927f8deb4111dee51968d83c47c88134de831f
mahdi <150438109+mahdikamyabi1999@users.noreply.github.com>
2024-02-20 21:00:03 +0330

    setup Fetch API an d get data from it

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,8 @@ import { NavLink } from "react-router-dom";
 import { GlobalContext } from "../context";
 
 function Navbar() {
-  const {searchParam,SetSearchParams} = useContext(GlobalContext);
+  const {searchParam,SetSearchParams,handleSubmit} = useContext(GlobalContext);
+  
   return (
     <nav className="flex justify-between items-center py-8 container mx-auto flex-col lg:flex-row gap-5 lg:gap-0">
       <h2 className="text-2xl font-semibold"><NavLink
@@ -12,11 +13,13 @@ function Navbar() {
         >
           Food Recipe
         </NavLink></h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
           name="search"
           placeholder="Enter items..."
+          value={searchParam}
+          onChange={e => SetSearchParams(e.target.value)}
           className="bg-white/75 p-3 px-8 rounded-full outline-none lg:w-96 shadow-lg shadow-red-100 focus:shadow-red-200"
         />
       </form>
--- src/context/index.jsx
+++ src/context/index.jsx
@@ -1,8 +1,33 @@
 import { createContext, useState } from "react";
 export const GlobalContext = createContext(null);
 
-
-export default function GlobalState({children}) {
-  const [searchParam,SetSearchParams] = useState('');
-  return <GlobalContext.Provider value={{searchParam,SetSearchParams}}>{children}</GlobalContext.Provider>
+export default function GlobalState({ children }) {
+  const [searchParam, SetSearchParams] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [recipeList, setRecipeList] = useState([]);
+  async function handleSubmit(event) {
+    event.preventDefault();
+    try {
+      const res = await fetch(
+        `https://forkify-api.herokuapp.com/api/v2/recipes?search=${searchParam}&key=fa1de4e3-0c5f-4dff-a8c1-87b11dc33cbb`
+      );
+      const data = await res.json();
+      if (data?.data?.recipes) {
+        setRecipeList(data?.data?.recipes);
+        setLoading(false);
+        SetSearchParams("");
+      }
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+      SetSearchParams("");
+    }
+  }
+  return (
+    <GlobalContext.Provider
+      value={{ searchParam, SetSearchParams, handleSubmit }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
 }
